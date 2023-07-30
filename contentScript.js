89db0171c104963335859b45faa00329c176ed65
mclovin <108329399+McLovin-RSPS@users.noreply.github.com>
2023-07-30 02:03:15 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,5 @@
 function extractTextWithNewlines(element) {
   let lines = [];
-
   function traverse(node) {
     for (let childNode of node.childNodes) {
       if (childNode.nodeType === Node.TEXT_NODE) {
@@ -13,36 +12,32 @@ function extractTextWithNewlines(element) {
       }
     }
   }
-
   traverse(element);
   return lines.join(" ");
 }
 
 function addButton() {
   if (window.location.host === "prompttoolbox.com") {
-    let clipButton = document.getElementById("kt_clipboardicon_0");
-
-    if (clipButton) {
-      let cardTitle = document.querySelector(".card-title");
-      if (cardTitle) {
+    let cardTitles = document.querySelectorAll(".card-title");
+    if (cardTitles) {
+      cardTitles.forEach((cardTitle) => {
         cardTitle.style.width = "90%";
-      }
+        let text = extractTextWithNewlines(cardTitle);
 
-      let text = extractTextWithNewlines(cardTitle);
+        let linkButton = document.createElement("button");
+        linkButton.className = "btn btn-icon btn-sm btn-light";
+        linkButton.innerHTML =
+          '<span class="svg-icon svg-icon-2"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21.4451171,17.7910156 C21.4451171,16.9707031 21.6208984,13.7333984 19.0671874,11.1650391 C17.3484374,9.43652344 14.7761718,9.13671875 11.6999999,9 L11.6999999,4.69307548 C11.6999999,4.27886191 11.3642135,3.94307548 10.9499999,3.94307548 C10.7636897,3.94307548 10.584049,4.01242035 10.4460626,4.13760526 L3.30599678,10.6152626 C2.99921905,10.8935795 2.976147,11.3678924 3.2544639,11.6746702 C3.26907199,11.6907721 3.28437331,11.7062312 3.30032452,11.7210037 L10.4403903,18.333467 C10.7442966,18.6149166 11.2188212,18.596712 11.5002708,18.2928057 C11.628669,18.1541628 11.6999999,17.9721616 11.6999999,17.7831961 L11.6999999,13.5 C13.6531249,13.5537109 15.0443703,13.6779456 16.3083984,14.0800781 C18.1284272,14.6590944 19.5349747,16.3018455 20.5280411,19.0083314 L20.5280247,19.0083374 C20.6363903,19.3036749 20.9175496,19.5 21.2321404,19.5 L21.4499999,19.5 C21.4499999,19.0068359 21.4451171,18.2255859 21.4451171,17.7910156 Z" transform="translate(12.254964, 11.721538) scale(-1, 1) translate(-12.254964, -11.721538) " fill="currentColor"/></svg></span>';
+        linkButton.style.marginRight = "5px"; // Add some margin to the button
 
-      let linkButton = document.createElement("button");
-      linkButton.className = "btn btn-icon btn-sm btn-light";
-      linkButton.innerHTML =
-        '<span class="svg-icon svg-icon-2"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21.4451171,17.7910156 C21.4451171,16.9707031 21.6208984,13.7333984 19.0671874,11.1650391 C17.3484374,9.43652344 14.7761718,9.13671875 11.6999999,9 L11.6999999,4.69307548 C11.6999999,4.27886191 11.3642135,3.94307548 10.9499999,3.94307548 C10.7636897,3.94307548 10.584049,4.01242035 10.4460626,4.13760526 L3.30599678,10.6152626 C2.99921905,10.8935795 2.976147,11.3678924 3.2544639,11.6746702 C3.26907199,11.6907721 3.28437331,11.7062312 3.30032452,11.7210037 L10.4403903,18.333467 C10.7442966,18.6149166 11.2188212,18.596712 11.5002708,18.2928057 C11.628669,18.1541628 11.6999999,17.9721616 11.6999999,17.7831961 L11.6999999,13.5 C13.6531249,13.5537109 15.0443703,13.6779456 16.3083984,14.0800781 C18.1284272,14.6590944 19.5349747,16.3018455 20.5280411,19.0083314 L20.5280247,19.0083374 C20.6363903,19.3036749 20.9175496,19.5 21.2321404,19.5 L21.4499999,19.5 C21.4499999,19.0068359 21.4451171,18.2255859 21.4451171,17.7910156 Z" transform="translate(12.254964, 11.721538) scale(-1, 1) translate(-12.254964, -11.721538) " fill="currentColor"/></svg></span>';
-      linkButton.style.marginRight = "5px"; // Add some margin to the button
+        linkButton.addEventListener("click", function () {
+          window.open(
+            `https://chat.openai.com/?prompt=${encodeURIComponent(text)}`
+          );
+        });
 
-      linkButton.addEventListener("click", function () {
-        window.open(
-          `https://chat.openai.com/?prompt=${encodeURIComponent(text)}`
-        );
+        cardTitle.parentNode.insertBefore(linkButton, cardTitle);
       });
-
-      clipButton.parentNode.insertBefore(linkButton, clipButton);
     }
   } else if (window.location.host === "chat.openai.com") {
     let oldUrl = "";
@@ -74,7 +69,7 @@ window.onload = function () {
   addButton();
 
   // Event listener for the "Clear Text" button
-  document.getElementById("clearButton").addEventListener("click", function () {
+  document.getElementById("clearButton")?.addEventListener("click", function () {
     let textarea = document.getElementById("prompt-textarea");
     if (textarea) {
       textarea.value = "";
@@ -82,7 +77,7 @@ window.onload = function () {
   });
 
   // Event listener for the "Copy All Text" button
-  document.getElementById("copyButton").addEventListener("click", function () {
+  document.getElementById("copyButton")?.addEventListener("click", function () {
     let textToCopy = document.body.innerText;
     if (textToCopy) {
       navigator.clipboard.writeText(textToCopy).then(function () {
